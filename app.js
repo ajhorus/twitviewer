df319c61b935b1427af9596a133981d2ae3ca358
Nestor Martinez <nestor.martinez@gmail.com>
2012-07-31 02:08:04 -0500

    Fix session issue. Succesfull validation using oauth

--- app.js
+++ app.js
@@ -17,9 +17,12 @@ app.configure(function(){
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.session({ secret: 'keyboard cat', key: 'sid'}));
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  
 });
 
 app.configure('development', function(){
@@ -27,6 +30,80 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+
+app.post('/', function(req,res) {
+req.session.rosas = "red rojas";
+console.log("session id req: " + req.session.id);
+console.log(req.session.rosas + "; 1 ses");
+res.redirect('/dashboard');
+res.send("hoo hee ha ha unimportant junk data");
+});
+
+app.get('/dashboard', function(req, res){
+    console.log("session id: " + req.session.id);
+    console.log("session in return " +  req.session.rosas);
+    res.render('dashboard',  {title: "testing dashboard"});
+});
+
+//**************************** twiter oauth   ********************************
+
+var OAuth= require('oauth').OAuth;
+
+var oa = new OAuth(
+    "https://api.twitter.com/oauth/request_token",
+	"https://api.twitter.com/oauth/access_token",
+	"U9GKXZfJMRru0ibAXMwog",
+	"bMuit8gE9fgTp58Hi4dXHQw1kucZQPDx00JJWPRCU",
+	"1.0",
+	"http://127.0.0.1:3000/auth/twitter/callback",
+	"HMAC-SHA1"
+);
+
+app.get('/auth/twitter', function(req, res){
+    oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+		if (error) {
+			console.log(error);
+			res.send("yeah no. didn't work.")
+		}
+		else {
+            req.session.idtemp = "999";
+			req.session.oauth = {};
+			req.session.oauth.token = oauth_token;
+			console.log('oauth.token: ' + req.session.oauth.token);
+			req.session.oauth.token_secret = oauth_token_secret;
+			console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
+			res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
+	}
+	});
+});
+
+app.get('/auth/twitter/callback', function(req, res, next){
+    if (req.session.oauth) {
+        
+		req.session.oauth.verifier = req.query.oauth_verifier;
+		var oauth = req.session.oauth;
+
+		oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier, 
+		function(error, oauth_access_token, oauth_access_token_secret, results){
+			if (error){
+				console.log(error);
+				res.send("yeah something broke.");
+			} else {
+				req.session.oauth.access_token = oauth_access_token;
+				req.session.oauth,access_token_secret = oauth_access_token_secret;
+				console.log(results);
+				var user_data = results;
+				console.log(user_data.screen_name);
+				res.render('dashboard', {title: user_data.screen_name});
+				
+			}
+		}
+		);
+	} else
+		next(new Error("you're not supposed to be here."))
+});
+//******************************************************************************
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
